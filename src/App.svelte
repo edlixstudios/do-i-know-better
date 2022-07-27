3b227d90a874dcd2734222a19a737f38c950fdc4
Eduard <info@edlixstudios.de>
2022-07-27 21:53:09 +0200

    add: nice transition

--- src/App.svelte
+++ src/App.svelte
@@ -40,11 +40,13 @@
 
 
 <div class="bg-slate-800 w-screen h-screen" > 
-  <div class="w-full h-full flex flex-col gap-16 justify-center items-center " >
-    {#if show}
-      <h1 in:slide class="text-slate-100 h-1/5 font-bold text-3xl text-center leading-normal md:text-5xl xl:text-7xl" >{screenText}</h1>
-    {/if}
-    <div class="flex flex-col gap-2" >
+  <div class="w-full h-full flex flex-col justify-center items-center " >
+    <div class="h-1/2 flex flex-col justify-center" >
+      {#if show}
+        <h1 in:slide class="text-slate-100 font-bold text-3xl text-center leading-normal md:text-5xl xl:text-7xl" >{screenText}</h1>
+      {/if}
+    </div>
+    <div class="flex flex-col gap-2 h-1/2 justify-center" >
       <button on:click={getAnswer} class="text-2xl font-bold text-slate-800 bg-slate-100 py-2 px-8 rounded-full transition-all md:text-4xl xl:font-sans xl:text-5xl xl:hover:scale-110 " >Let's find out</button>
       <button on:click={reset} class="text-slate-200 text-lg md:text-1xl xl:text-3xl" >Wanna try again?</button>
     </div>
