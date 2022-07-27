b37e66d6b0352a793a409e8896edb755dee25e1f
Eduard <info@edlixstudios.de>
2022-07-27 21:20:48 +0200

    ok

--- src/App.svelte
+++ src/App.svelte
@@ -27,10 +27,10 @@
 
 <div class="bg-slate-800 w-screen h-screen" > 
   <div class="w-full h-full flex flex-col gap-16 justify-center items-center " >
-    <h1  class="text-slate-100 h-1/3 font-bold text-3xl text-center leading-normal  " >{screenText}</h1>
+    <h1  class="text-slate-100 h-1/5 font-bold text-3xl text-center leading-normal md:text-5xl xl:text-7xl" >{screenText}</h1>
     <div class="flex flex-col gap-2" >
-      <button on:click={getAnswer} class="text-2xl font-bold text-slate-800 bg-slate-100 py-2 px-8 rounded-full" >Let's find out</button>
-      <button on:click={reset} class="text-slate-200" >Wanna try again?</button>
+      <button on:click={getAnswer} class="text-2xl font-bold text-slate-800 bg-slate-100 py-2 px-8 rounded-full md:text-4xl xl:text-6xl" >Let's find out</button>
+      <button on:click={reset} class="text-slate-200 text-lg md:text-1xl xl:text-3xl" >Wanna try again?</button>
     </div>
   </div>
 </div>
